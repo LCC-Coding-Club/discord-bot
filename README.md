b96a78a4e085623b0542a3667833ff7aae8d3e69
Lower Columbia Collage Coding Club <cahillj2@my.lowercolumbia.edu>
2025-12-24 16:28:11 -0800

    Init commit
    This is an initial commit with a very basic bot. 
    .env file is missing due to sensitive content.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # discord-bot
+This is the LCC Coding clubs offical discord bot.
+
+This is a collabritve effort for the members.
+If you are a member and want to contribute please check the discord.
---
+++ main.py
@@ -0,0 +1,53 @@
+import discord
+from discord.ext import commands
+import logging
+from dotenv import load_dotenv
+import os
+
+# documentation reference: https://discordpy.readthedocs.io/en/stable/
+# Video used to set up bot: https://www.youtube.com/watch?v=YD_N6Ffoojw
+# You might have to look up some things on your own. Google is a valuable resource.
+
+
+### Your setup ###
+# Install python: https://www.python.org/downloads/
+# download the github repo https://github.com/LCC-Coding-Club/discord-bot
+# run pip install -r .\requirements.txt
+# your IDE might require additional setup to run the bot. Try to use google first. If you still cant figure it out, ask for help on discord.
+
+# if you need to test something get the .env file from a club officer.
+# Do NOT share the .env file with anyone outside of club officers.
+
+# if you have the .env file, be careful when you run the bot.
+# If the bot is running in the server DO NOT RUN ANOTHER INSTANCE. 
+# Only one instance of the bot can run at a time.
+
+
+#loads and sets environment discord token
+load_dotenv()
+token = os.getenv("DISCORD-TOKEN")
+
+#sets up logging
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+
+#sets up bot intents
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+#creates bot instance
+bot = commands.Bot(command_prefix='>', intents=intents)
+
+#logs a message when bot starts up, we can possibly use this for other things later
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user.name}')
+
+#simple hello command
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f"Hello {ctx.author.mention}!")
+
+#log_levels: NOTSET(0), DEBUG(10), INFO(20), WARNING(30), ERROR(40), CRITICAL(50)
+#We cant use logging.debug for reasons
+bot.run(token,log_handler=handler,log_level=10)
